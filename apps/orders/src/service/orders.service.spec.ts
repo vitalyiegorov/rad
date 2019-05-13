d4c917b84a5e7e60ef88e1211ae82ddec210c4e4
Vitalii Yehorov <vitalyiegorov@gmail.com>
2019-05-14 00:53:17 +0300

    Finished order service unit tests

--- apps/orders/src/service/orders.service.spec.ts
+++ apps/orders/src/service/orders.service.spec.ts
@@ -1,20 +1,27 @@
 import { OrdersService } from './orders.service';
-import { Repository } from 'typeorm';
+import { OrderStatusEnum } from '@app/common';
 
 let ordersService: OrdersService;
+const orderMock = { id: 1, total: 0, user: '', status: OrderStatusEnum.CREATED };
+const orderDeliveredMock = { id: 2, total: 0, user: '', status: OrderStatusEnum.DELIVERED };
 
-const createMock = jest.fn((dto: any) => {
-  return dto;
+const createMock = jest.fn(() => {
+  return orderMock;
 });
 
 const saveMock = jest.fn((dto: any) => {
   return dto;
 });
 
+const findMock = jest.fn((id: number) => {
+  return id === orderMock.id ? orderMock : orderDeliveredMock;
+});
+
 const MockRepository = jest.fn().mockImplementation(() => {
   return {
     create: createMock,
-    save: saveMock
+    save: saveMock,
+    findOneOrFail: findMock
   };
 });
 
@@ -26,16 +33,20 @@ describe('Orders service', () => {
   });
 
   it('should create new Order', async () => {
-    await ordersService.create();
-
-    expect(true).toEqual(true);
+    expect(await ordersService.create()).toEqual(orderMock);
   });
 
-  it('should cancel order by id', () => {
-    expect(true).toEqual(true);
+  it('should cancel order by id', async () => {
+    const order = await ordersService.cancel(orderMock.id);
+
+    expect(order.status).toEqual(OrderStatusEnum.CANCELED);
   });
 
-  it('should show order status by id', () => {
-    expect(true).toEqual(true);
+  it('should fail canceling order on wrong order status', async () => {
+    await expect(ordersService.cancel(orderDeliveredMock.id)).rejects.toThrow(Error);
+  });
+
+  it('should show order status by id', async () => {
+    expect(await ordersService.status(orderMock.id)).toEqual(orderMock.status);
   });
 });
